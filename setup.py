b3e3af062e4334f21b65f22aeda3589341f6645c
Nate Maxfield <nate@clypd.com>
2022-04-12 18:06:16 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='xandr-statuspageio',
-    version='0.0.5',
+    version='0.0.8',
     description='StatusPage.io API V1 library client for Python',
     long_description=README,
     author='Nate Maxfield',
