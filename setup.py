ef28935f6c7bc46f39108cc9fede66b6883b9444
Nate Maxfield <nate@clypd.com>
2022-04-12 17:23:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 
 setup(
-    name='statuspageio',
+    name='statuspage_io',
     version='0.0.4',
     description='StatusPage.io API V1 library client for Python',
     long_description=README,
